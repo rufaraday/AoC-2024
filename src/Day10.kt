23d49318daeeed5fcc48bfcb7c83ab11959271df
Rafal Orlowski <rafal.orlowski@partner.bmw.de>
2024-12-10 08:08:51 +0100

    [D10P1] Part 2 algorithm

--- src/Day10.kt
+++ src/Day10.kt
@@ -79,7 +79,7 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         var sum = 0
-        var trailStarts : MutableSet<Pair<Int, Int>> = emptySet<Pair<Int, Int>>().toMutableSet()
+        val trailStarts : MutableSet<Pair<Int, Int>> = emptySet<Pair<Int, Int>>().toMutableSet()
         val area : MutableList<List<Int>> = emptyList<List<Int>>().toMutableList()
         println(input.size)
         for (i in 0..input.lastIndex) {
@@ -95,8 +95,100 @@ fun main() {
         return sum
     }
 
+    fun checkDistinctTrail(
+        area: List<List<Int>>,
+        trailStart: Pair<Int, Int>
+    ): List<Pair<Int, Int>> {
+        val startX = trailStart.first
+        val startY = trailStart.second
+        val startVal = area[startY][startX]
+        var dX: Int
+        var dY: Int
+        val trails : MutableList<Pair<Int, Int>> = emptyList<Pair<Int, Int>>().toMutableList()
+        println("check ${startX to startY}")
+        try {
+            dX = startX + 1
+            dY = startY
+            if (area[dY][dX] == startVal + 1) {
+                if (area[dY][dX] == 9) {
+                    println("add ${dX to dY}")
+                    trails.add(dX to dY)
+                } else {
+                    trails.addAll(checkDistinctTrail(area, dX to dY))
+                }
+            }
+        } catch (e: java.lang.IndexOutOfBoundsException) {
+            // out of area
+        }
+        try {
+            dX = startX - 1
+            dY = startY
+            if (area[dY][dX] == startVal + 1) {
+                if (area[dY][dX] == 9) {
+                    trails.add(dX to dY)
+                    println("add ${dX to dY}")
+                } else {
+                    trails.addAll(checkDistinctTrail(area, dX to dY))
+                }
+            }
+        } catch (e: java.lang.IndexOutOfBoundsException) {
+            // out of area
+        }
+        try {
+            dX = startX
+            dY = startY + 1
+            if (area[dY][dX] == startVal + 1) {
+                if (area[dY][dX] == 9) {
+                    trails.add(dX to dY)
+                    println("add ${dX to dY}")
+                } else {
+                    trails.addAll(checkDistinctTrail(area, dX to dY))
+                }
+            }
+        } catch (e: java.lang.IndexOutOfBoundsException) {
+            // out of area
+        }
+        try {
+            dX = startX
+            dY = startY - 1
+            if (area[dY][dX] == startVal + 1) {
+                if (area[dY][dX] == 9) {
+                    trails.add(dX to dY)
+                    println("add ${dX to dY}")
+                } else {
+                    trails.addAll(checkDistinctTrail(area, dX to dY))
+                }
+            }
+        } catch (e: java.lang.IndexOutOfBoundsException) {
+            // out of area
+        }
+        return trails
+    }
+
+    fun trailDistinctScore(area: List<List<Int>>, trailStart: Pair<Int, Int>): Int {
+        println("trailStart = $trailStart")
+        val tops : MutableList<Pair<Int, Int>> = emptyList<Pair<Int, Int>>().toMutableList()
+        tops += checkDistinctTrail(area, trailStart)
+        println("trailStart = $trailStart : $tops")
+        return tops.size
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        val trailStarts : MutableSet<Pair<Int, Int>> = emptySet<Pair<Int, Int>>().toMutableSet()
+        val area : MutableList<List<Int>> = emptyList<List<Int>>().toMutableList()
+        println(input.size)
+        for (i in 0..input.lastIndex) {
+            println(input[i])
+            area.add(input[i].map { it.digitToInt() })
+            for (j in 0..area[i].lastIndex) {
+                if (area[i][j] == 0) {
+                    trailStarts.add(j to i)
+                }
+            }
+        }
+        trailStarts.forEach { sum += trailDistinctScore(area.toList(), it) }
+        return sum
     }
 
     // Test if implementation meets criteria from the description, like:
@@ -105,6 +197,7 @@ fun main() {
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day10_test")
     check(part1(testInput) == 36)
+    check(part2(testInput) == 81)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day10")
