eedc60be29678e87d45ad539dddc5d80964ab9b4
Rafal Orlowski <rafal.orlowski@partner.bmw.de>
2024-12-10 08:55:15 +0100

    [D09P1] 1960677604 is too low

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,6 +1,43 @@
+import java.math.BigInteger
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val disc = emptyList<Int>().toMutableList()
+        // read disc
+        var id = 0
+        for(i in 0..input[0].lastIndex) {
+            for(j in 0..<input[0][i].digitToInt()) {
+                if (i.rem(2) == 0) {
+                    disc.add(id)
+                } else {
+                    disc.add(-1)
+                }
+            }
+            if (i.rem(2) == 0) {
+                id++
+            }
+        }
+//        println(disc)
+        // defrag disc
+        for (i in 0..disc.lastIndex) {
+            if (disc[i] >= 0) continue
+            for (j in disc.lastIndex downTo i) {
+                if (disc[j] >= 0) {
+                    disc[i] = disc[j]
+                    disc[j] = -1
+                    break
+                }
+            }
+//            println(disc)
+        }
+        // calculate checksum
+        var checksum = 0
+        for (i in 0..disc.lastIndex) {
+            if (disc[i] > 0) {
+                checksum += i * disc[i]
+            }
+        }
+        return checksum
     }
 
     fun part2(input: List<String>): Int {
