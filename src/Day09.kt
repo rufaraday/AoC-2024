01190f0475200d2eb8feea086a3063199aa94412
Rafal Orlowski <rafal.orlowski@partner.bmw.de>
2024-12-10 08:20:42 +0100

    [D09P1] Day 9 init corrected

--- src/Day09.kt
+++ src/Day09.kt
@@ -8,14 +8,14 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+//    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day09_test")
+    check(part1(testInput) == 1928)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("Day09")
     part1(input).println()
     part2(input).println()
 }
