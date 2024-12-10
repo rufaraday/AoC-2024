72f26c7ed265b1c069d435a13ca2974eb7940911
Rafal Orlowski <rafal.orlowski@partner.bmw.de>
2024-12-10 23:05:00 +0100

    [D07P2] Gold star

--- src/Day07.kt
+++ src/Day07.kt
@@ -30,8 +30,37 @@ fun main() {
         return accu
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        println("part2")
+        val calibrations = emptyList<Pair<Long, List<Int>>>().toMutableList()
+        var accu : Long = 0
+        input.forEach() {
+            val numbers = it.split(" ")
+            val calibration = numbers.first().dropLast(1).toLong() to numbers.drop(1).map{ it.toInt()}
+            calibrations.add(calibration)
+            println("result = ${calibration.first}, numbers(${calibration.second.size}) = ${calibration.second}")
+            for(i in 0..(3.toDouble().pow(calibration.second.lastIndex)-1).toInt()) {
+                val operations = i.toString(3).padStart(calibration.second.lastIndex, '0')
+                println(operations)
+                var result = calibration.second[0].toLong()
+                for (j in 1..calibration.second.lastIndex) {
+                    if (operations[j-1] == '0') {
+                        result += calibration.second[j]
+                    } else if (operations[j-1] == '1') {
+                        result *= calibration.second[j]
+                    } else {
+                        result = "$result${calibration.second[j]}".toLong()
+                    }
+                }
+                if (result == calibration.first) {
+                    accu += result
+                    break
+                }
+            }
+            println()
+        }
+        println(calibrations)
+        return accu
     }
 
     // Test if implementation meets criteria from the description, like:
@@ -40,6 +69,7 @@ fun main() {
     // Or read a large test input from the `src/Day07_test.txt` file:
     val testInput = readInput("Day07_test")
     check(part1(testInput) == 3749.toLong())
+    check(part2(testInput) == 11387.toLong())
 
     // Read the input from the `src/Day07.txt` file.
     val input = readInput("Day07")
