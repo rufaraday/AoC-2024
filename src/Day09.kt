b8f4e1b2e5d74e6343872078bc4d794c263466f9
Rafal Orlowski <rafal.orlowski@partner.bmw.de>
2024-12-10 13:42:59 +0100

    [D09P2] Does not work yet
    Bug: needs to move free space and combine it with neighboring free spaces

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,5 +1,3 @@
-import java.math.BigInteger
-
 fun main() {
     fun part1(input: List<String>): Long {
         val disc = emptyList<Int>().toMutableList()
@@ -18,7 +16,7 @@ fun main() {
             }
         }
 //        println(disc)
-        // defrag disc
+        // move files
         for (i in 0..disc.lastIndex) {
             if (disc[i] >= 0) continue
             for (j in disc.lastIndex downTo i) {
@@ -40,8 +38,79 @@ fun main() {
         return checksum
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun printDisc(free: List<Triple<Int, Int, Int>>, files: List<Triple<Int, Int, Int>>) {
+        val discElements = (free + files).toMutableList()
+        val disc = mutableListOf<Int>()
+        discElements.sortWith(compareBy {it.first})
+        var index = 0
+        discElements.forEach() {
+            if (it.first == index) {
+                for (i in 0..<it.second) {
+                    disc.add(it.third)
+                }
+            } else {
+                println("error writing $it to $disc")
+                throw RuntimeException()
+            }
+            index += it.second
+        }
+        println("disc: $disc")
+    }
+
+    fun part2(input: List<String>): Long {
+        val disc = emptyList<Int>().toMutableList()
+        // read disc
+        val freeMap = mutableListOf<Triple<Int, Int, Int>>()
+        val fileMap = mutableListOf<Triple<Int, Int, Int>>()
+        var discIndex = 0
+        for(i in 0..input[0].lastIndex) {
+            if (i.rem(2) == 0) {
+                fileMap.add(Triple(discIndex, input[0][i].digitToInt(), i.div(2)))
+            } else {
+                freeMap.add(Triple(discIndex, input[0][i].digitToInt(), -1))
+            }
+            discIndex += input[0][i].digitToInt()
+        }
+        println("free=$freeMap")
+        println("file=$fileMap")
+//        println(disc)
+        // defrag move files
+        for (i in fileMap.size-1 downTo 1) {
+            var j = 0
+            while (j < freeMap.size) {
+                println("i=$i, j=$j")
+                if (fileMap[i].second <= freeMap[j].second) {
+                    println("${fileMap[i]} fits into ${freeMap[j]}")
+                    fileMap[i] = Triple(freeMap[j].first, fileMap[i].second, fileMap[i].third)
+                    if (fileMap[i].second != freeMap[j].second) {
+                        val diff = freeMap[j].second - fileMap[i].second
+                        freeMap[j] = Triple(freeMap[j].first + fileMap[i].second, diff, -1)
+                        println("Put ${fileMap[i]} into ${freeMap[j]}, remain $diff")
+                        break
+                    } else {
+                        println("remove ${freeMap[j]}")
+                        freeMap.removeAt(j)
+                        break
+                    }
+                }
+                j++
+                println("free=$freeMap")
+                println("file=$fileMap")
+                printDisc(freeMap.toList(), fileMap.toList())
+//                Thread.sleep(1000)
+            }
+        }
+        println("free=$freeMap")
+        fileMap.sortWith(compareBy {it.first})
+        println("sorted files=$fileMap")
+        // calculate checksum
+        var checksum : Long = 0
+        for (i in 0..disc.lastIndex) {
+            if (disc[i] > 0) {
+                checksum += i * disc[i]
+            }
+        }
+        return checksum
     }
 
     // Test if implementation meets criteria from the description, like:
@@ -50,6 +119,7 @@ fun main() {
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day09_test")
     check(part1(testInput) == 1928.toLong())
+    check(part2(testInput) == 2858.toLong())
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day09")
