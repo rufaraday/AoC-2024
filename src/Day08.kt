1494df8aab2023502db81ef6bc2c212c1f0c836e
Rafal Orlowski <rafal.orlowski@partner.bmw.de>
2024-12-11 16:38:43 +0100

    [D08P2] Part 2 init

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,4 +1,17 @@
 fun main() {
+    fun inBounds(ap1: Pair<Int, Int>, boundX: Int, boundY: Int) =
+        ap1.first in 0..<boundX && ap1.second in 0..<boundY
+
+    fun plusDelta(
+        p1: Pair<Int, Int>,
+        d: Pair<Int, Int>
+    ) = (p1.first + d.first) to (p1.second + d.second)
+
+    fun minusDelta(
+        p2: Pair<Int, Int>,
+        d: Pair<Int, Int>
+    ) = (p2.first - d.first) to (p2.second - d.second)
+
     fun part1(input: List<String>): Int {
         val antinodes = mutableSetOf<Pair<Int, Int>>()
         val antennas = mutableMapOf<Char, MutableList<Pair<Int, Int>>>()
@@ -20,13 +33,13 @@ fun main() {
                     if (i != j) {
                         val p1 = antenna.value[i]
                         val p2 = antenna.value[j]
-                        val d = (p1.first - p2.first) to (p1.second - p2.second)
-                        val ap1 = (p1.first + d.first) to (p1.second + d.second)
-                        val ap2 = (p2.first - d.first) to (p2.second - d.second)
-                        if (ap1.first in 0..<boundX && ap1.second in 0..<boundY) {
+                        val d = minusDelta(p1, p2)
+                        val ap1 = plusDelta(p1, d)
+                        val ap2 = minusDelta(p2, d)
+                        if (inBounds(ap1, boundX, boundY)) {
                             antinodes += ap1
                         }
-                        if (ap2.first in 0..<boundX && ap2.second in 0..<boundY) {
+                        if (inBounds(ap2, boundX, boundY)) {
                             antinodes += ap2
                         }
                     }
@@ -38,7 +51,41 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val antinodes = mutableSetOf<Pair<Int, Int>>()
+        val antennas = mutableMapOf<Char, MutableList<Pair<Int, Int>>>()
+        val boundX = input[0].length
+        val boundY = input.size
+        for (y in input.indices) {
+            for (x in input[y].indices) {
+                if (input[y][x] != '.') {
+                    if (!antennas.contains(input[y][x])) {
+                        antennas[input[y][x]] = mutableListOf()
+                    }
+                    antennas[input[y][x]]?.add(Pair(x, y))
+                }
+            }
+        }
+        antennas.forEach() {antenna ->
+            for (i in antenna.value.indices) {
+                for (j in antenna.value.indices) {
+                    if (i != j) {
+                        val p1 = antenna.value[i]
+                        val p2 = antenna.value[j]
+                        val d = p1.minus(p2)
+                        val ap1 = plusDelta(p1, d)
+                        val ap2 = p2.minus(d)
+                        if (inBounds(ap1, boundX, boundY)) {
+                            antinodes += ap1
+                        }
+                        if (inBounds(ap2, boundX, boundY)) {
+                            antinodes += ap2
+                        }
+                    }
+                }
+            }
+        }
+        println(antennas)
+        return antinodes.size
     }
 
     // Test if implementation meets criteria from the description, like:
@@ -52,4 +99,8 @@ fun main() {
     val input = readInput("Day08")
     part1(input).println()
     part2(input).println()
+}
+
+private fun Pair<Int, Int>.minus(p: Pair<Int, Int>): Pair<Int, Int> {
+    return (this.first - p.first) to (this.second - p.second)
 }
