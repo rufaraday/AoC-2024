24285b9f3cf594aa7dfd3f58ce67e0dac6531759
Rafal Orlowski <rafal.orlowski@partner.bmw.de>
2024-12-13 07:06:06 +0100

    [D13P2] Part 2 first attempt
    Probably stuck in an infinite loop

--- src/Day13.kt
+++ src/Day13.kt
@@ -28,8 +28,43 @@ fun main() {
         return cost
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        // What is the fewest tokens you would have to spend to win all possible prizes?
+        // You estimate that each button would need to be pressed no more than 100 times to win a prize.
+        var cost = 0L
+        for (i in 0.. input.size / 4) {
+            val (dXA, dYA) = "Button A: X\\+(\\d+), Y\\+(\\d+)".toRegex().find(input[4*i])!!.destructured
+            val (dXB, dYB) = "Button B: X\\+(\\d+), Y\\+(\\d+)".toRegex().find(input[4*i + 1])!!.destructured
+            val (xP, yP) = "Prize: X=(\\d+), Y=(\\d+)".toRegex().find(input[4*i + 2])!!.destructured
+            val solutions = mutableListOf<Pair<Long, Long>>()
+            var min = Long.MAX_VALUE
+            val pX = xP.toLong() + 10000000000000
+            val pY = yP.toLong() + 10000000000000
+            var a = 0L
+            var b = 0L
+            var cX = 0L
+            var cY = 0L
+            while (cX < pX && cY < pY) {
+                a++
+                b = 0L
+                while (cX < pX && cY < pY) {
+                    cX = a * dXA.toInt() + b * dXB.toInt()
+                    cY = a * dYA.toInt() + b * dYB.toInt()
+                    if (cX == pX && cY == pY) {
+                        solutions += a to b
+                        if (3 * a + b < min) {
+                            min = 3 * a + b
+                        }
+                    }
+                    b++
+                }
+            }
+            if (min == Long.MAX_VALUE) {
+                min = 0
+            }
+            cost += min
+        }
+        return cost
     }
 
     // Test if implementation meets criteria from the description, like:
@@ -56,6 +91,7 @@ fun main() {
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day13_test")
     check(part1(testInput) == 480)
+//    check(part2(testInput) == 480L)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day13")
