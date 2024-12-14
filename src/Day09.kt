57dfe79423d1e07337d21edb46b14c36cc77a9bf
Rafal Orlowski <rafal.orlowski@partner.bmw.de>
2024-12-15 00:52:31 +0100

    [D09P2] Still not there
    Works with the test data but crashes on real input.

--- src/Day09.kt
+++ src/Day09.kt
@@ -77,21 +77,39 @@ fun main() {
         // defrag move files
         for (i in fileMap.size-1 downTo 1) {
             var j = 0
-            while (j < freeMap.size) {
+            freeMap.sortWith(compareBy {it.first})
+            while (freeMap[j].first < fileMap[i].first) {
                 println("i=$i, j=$j")
                 if (fileMap[i].second <= freeMap[j].second) {
                     println("${fileMap[i]} fits into ${freeMap[j]}")
+                    val indexCache = fileMap[i].first
+                    // Move file
                     fileMap[i] = Triple(freeMap[j].first, fileMap[i].second, fileMap[i].third)
+                    // Fix free space
                     if (fileMap[i].second != freeMap[j].second) {
                         val diff = freeMap[j].second - fileMap[i].second
                         freeMap[j] = Triple(freeMap[j].first + fileMap[i].second, diff, -1)
-                        println("Put ${fileMap[i]} into ${freeMap[j]}, remain $diff")
-                        break
+                        println("Put ${Triple(indexCache, fileMap[i].second, fileMap[i].third)} into ${Triple(fileMap[i].first, fileMap[i].second+diff, freeMap[j].third)}; free space now in ${freeMap[j]}, remain $diff")
+                        freeMap.add(Triple(indexCache, fileMap[i].second, -1))
+                        println("Added free space ${Triple(indexCache, fileMap[i].second, -1)}")
                     } else {
-                        println("remove ${freeMap[j]}")
-                        freeMap.removeAt(j)
-                        break
+                        freeMap[j] = Triple(indexCache, fileMap[i].second, -1)
+                        println("Moved ${Triple(indexCache, fileMap[i].second, fileMap[i].third)} to ${fileMap[i].first}; free space now in ${freeMap[j]}")
                     }
+                    // Fix free space
+                    var k = 1
+                    while (k in 1..freeMap.lastIndex) {
+                        if (freeMap[k-1].first + freeMap[k-1].second == freeMap[k].first) {
+                            // Merge k and k-1 element
+                            print("Merged ${freeMap[k-1]} and ${freeMap[k]} ")
+                            freeMap[k-1] = Triple(freeMap[k-1].first ,freeMap[k-1].second + freeMap[k].second ,-1)
+                            println("into ${freeMap[k-1]}")
+                            freeMap.removeAt(k)
+                            k--
+                        }
+                        k++
+                    }
+                    break
                 }
                 j++
                 println("free=$freeMap")
@@ -103,13 +121,16 @@ fun main() {
         println("free=$freeMap")
         fileMap.sortWith(compareBy {it.first})
         println("sorted files=$fileMap")
+        printDisc(freeMap.toList(), fileMap.toList())
         // calculate checksum
         var checksum : Long = 0
-        for (i in 0..disc.lastIndex) {
-            if (disc[i] > 0) {
-                checksum += i * disc[i]
+        fileMap.forEach {file ->
+            for(i in 0..<file.second) {
+                println("index ${file.first + i} file id ${file.third}")
+                checksum += (file.first + i) * file.third.toLong()
             }
         }
+        println(checksum)
         return checksum
     }
 
