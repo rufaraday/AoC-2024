da98f6eb01af2e916408cbc9a1084753afa7e946
Rafal Orlowski <rafal.orlowski@partner.bmw.de>
2024-12-15 23:29:33 +0100

    [D15P1] Silver star

--- src/Day15.kt
+++ src/Day15.kt
@@ -15,6 +15,18 @@ fun main() {
         return x to y
     }
 
+    fun calculateGpsSum(area: MutableList<MutableList<Char>>): Int {
+        var sum = 0
+        for (y in 0..area.lastIndex) {
+            for (x in 0..area[y].lastIndex) {
+                if (area[y][x] == 'O') {
+                    sum += 100 * y + x
+                }
+            }
+        }
+        return sum
+    }
+
     fun part1(input: List<String>): Int {
         val area = mutableListOf<MutableList<Char>>()
         val moves = mutableListOf<Char>()
@@ -30,12 +42,13 @@ fun main() {
                 }
             }
         }
-        printCharMatrix(area)
-        println("moves sequence: $moves")
+//        printCharMatrix(area)
+//        println("moves sequence: $moves")
         var robotPos = findPosition('@', area)
-        println(robotPos)
+//        println(robotPos)
         // Move
         moves.forEach {move ->
+//            println("$robotPos $move")
             val vector = when (move) {
                 'v' -> 0 to 1
                 '^' -> 0 to -1
@@ -43,11 +56,35 @@ fun main() {
                 '<' -> -1 to 0
                 else -> 0 to 0
             }
-            var movingElements = mutableListOf(robotPos)
-            val nextPos = robotPos.plus(vector)
-            area[nextPos.second][nextPos.first]
+            val movingElements = mutableListOf(robotPos)
+            var nextPos = robotPos.plus(vector)
+            while(area[nextPos.second][nextPos.first] !in listOf('.', '#')) {
+                movingElements.add(nextPos)
+                nextPos = nextPos.plus(vector)
+            }
+//            println(movingElements)
+//            println("$nextPos : ${area[nextPos.second][nextPos.first]}")
+            if (area[nextPos.second][nextPos.first] == '.') {
+//                println("moving elements $movingElements in direction $move ($vector)")
+                movingElements.reversed().forEach {
+                    val next = it.plus(vector)
+//                    println("  moving $it (${area[it.second][it.first]}) to $next (${area[next.second][next.first]})")
+                    area[next.second][next.first] = area[it.second][it.first]
+//                    println("  changing $it (${area[it.second][it.first]}) to \'.\'")
+                    area[it.second][it.first] = '.'
+                }
+//                robotPos = findPosition('@', area)
+//                println("old robot position $robotPos")
+                robotPos = robotPos.plus(vector)
+//                println("new robot position $robotPos")
+            } else {
+//                println("leaving elements $movingElements in place")
+            }
+//            printCharMatrix(area)
+//            println()
         }
-        return input.size
+        // Calculate sum of all boxes' GPS coordinates
+        return calculateGpsSum(area)
     }
 
     fun part2(input: List<String>): Int {
