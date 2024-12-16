2de8d32ef5536f8523dde8eafbf6c820527cb4b4
Rafal Orlowski <rafal.orlowski@partner.bmw.de>
2024-12-16 23:29:22 +0100

    [D16P1] Read map
    And correct filenames.

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,5 +1,10 @@
 fun main() {
     fun part1(input: List<String>): Int {
+        val area = mutableListOf<MutableList<Char>>()
+        input.forEach {
+            area.add(it.toMutableList())
+        }
+        printCharMatrix(area)
         return input.size
     }
 
@@ -11,13 +16,13 @@ fun main() {
 //    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val firstTestInput = readInput("Day01_test_first")
+    val firstTestInput = readInput("Day16_test_first")
     check(part1(firstTestInput) == 7036)
-    val secondTestInput = readInput("Day01_test_first")
+    val secondTestInput = readInput("Day16_test_first")
     check(part1(secondTestInput) == 11048)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("Day16")
     part1(input).println()
     part2(input).println()
 }
